7c024ec7671466ac03482ec360effcd63eabaa65
Trevor Makausi <makausithomas@gmail.com>
2022-03-13 03:54:14 +0200

    feat: add static analysis and pre-commit hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <div> This is a new react app</div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<div> This is a new react app</div>, document.getElementById('root'));
